3803a5a7bbcdcbb62ffd5fbf5919b0d6fd6039e1
SoumaySoni <soumaysoni1687@gmail.com>
2025-01-18 12:55:38 +0530

    update LargestElementInArray.js

--- LargestElementInArray.js
+++ LargestElementInArray.js
@@ -1,6 +1,6 @@
 // Write a JavaScript function that takes an array of numbers as input and returns the largest number in the array.
 
-const largestElementInArray = (array) => {
+var largestElementInArray = (array) => {
   if (array.length === 0) return "Array is empty"; // Handle empty array
 
   let largestElement = array[0]; // Initialize with the first element of the array
@@ -16,3 +16,16 @@ const largestElementInArray = (array) => {
 console.log(largestElementInArray([100, 20, 300]));
 console.log(largestElementInArray([-10, -20, -3]));
 console.log(largestElementInArray([]));
+
+
+// Using Reduce method for more efficiency ...........................................
+
+var largestElementInArray = (array) => {
+    if (array.length === 0) return "Array is empty"; // Handle empty array
+
+    return array.reduce((max, current) => (current > max ? current : max));
+};
+
+console.log(largestElementInArray([100, 20, 300]));  // Outputs: 300
+console.log(largestElementInArray([-10, -20, -3]));  // Outputs: -3
+console.log(largestElementInArray([]));             // Outputs: "Array is empty"
